60ecd6c3c483602083a228cf3a990f1e73c7c4e7
Presidomusli <40693104+Presidomusli@users.noreply.github.com>
2018-06-29 23:45:28 +0100

    design.js

--- designs.js
+++ designs.js
@@ -1,21 +1,44 @@
-$(document).ready(function() {
-	$('#sizePicker').submit(function makeGrid(grid)
-	{
-		$('table tr').remove();
-		var rows = $('#inputHeight').val();
-		var cols = $('#inputWidth').val();
-			for (var i = 1; i<= rows; i++) {
-				$('table').append("<tr></tr>");
-				for (var j = 1; j <= cols; j++) {
-					$('tr:last').append("<td></td>");
-					$('td').attr("class", 'cells');
-				}
-			}
-			grid.preventDefault();
-		
-			$('.cells').click(function(event) {
-				var paint = $('#colorPicker').val();
-				$('event.target').css('background-color', paint);
-			});
-	});
-});
+var rows, cols;
+
+$( '#sizePicker' ).submit( function( evt ) {
+
+ evt.preventDefault();
+
+ makeGrid();
+
+})
+
+function makeGrid() {
+
+ rows = $( '#inputHeight' ).val();
+
+ cols = $( '#inputWeight' ).val();
+
+ $('#pixelCanvas').children().remove();
+
+ for ( var i = 0; i < rows; i++ ){
+
+     $('#pixelCanvas').append( '<tr></tr>' );
+
+ }  for ( var j = 0; j < cols; j++ ){
+
+     $( 'tr' ).append( '<td></td>' );
+
+ } 
+ var cell =   $('#pixelCanvas').find( 'td' );  
+ 		cell.click( function () { 
+
+ var paint = $( '#colorPicker' ).val();
+
+ if ( $( this ).attr('style') ){
+
+       $( this ).removeAttr('style');
+
+     }else{
+
+       $( this ).css('background-color', paint);
+
+     }
+
+ });
+}
